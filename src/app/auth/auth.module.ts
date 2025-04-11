5fc383c41b23fe591f5a599fae5cc7dd237a7928
hisham-mhammed-afifi <hisham.mohmmed@gmail.com>
2025-04-12 01:18:04 +0200

    redirect to login page as default auth route

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -19,6 +19,11 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent,
       },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
     ],
   },
 ];
